3af32214660e80799f041164db12954c7d7633c5
bonghee <hae8064@naver.com>
2022-12-18 22:53:18 +0900

    리액트 회원가입 데이터 서버에서 받아오기 성공

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 const http = require('http').createServer(app);
-//__dirname 선언
-// const __dirame = path.resolve();
+const bodyParser = require('body-parser');
 
 app.use(express.json());
 
@@ -13,13 +12,18 @@ app.use(express.static(path.join(__dirname, '/variousproj/build')));
 app.use(cors());
 
 //메인페이지 접속시 build 폴더의 index.html을 보내줘!
-app.get('/', (res, req) => {
-  req.sendFile(path.join(__dirname, '/variousproj/build/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/variousproj/build/index.html'));
+});
+
+app.post('/signUp', (req, res) => {
+  let datas = req.body.signUpDatas;
+  console.log(datas);
 });
 
 //라우팅은 리액트가 담당하도록 설계한다.
-app.get('*', (res, req) => {
-  req.sendFile(path.join(__dirname, '/variousproj/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/variousproj/build/index.html'));
 });
 
 http.listen(8080, () => {
