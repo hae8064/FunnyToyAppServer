8f9bebb227c8c00c5fead85174c45d143d86eede
bonghee <hae8064@naver.com>
2022-12-21 01:15:45 +0900

    로그인 데이터를 가져와 암호화된 비밀번호를 compare후 match가 true면 클라이언트에 데이터 success보낸다.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const { sequelize } = require('./models');
 const { User } = require('./models');
 const bcrypt = require('bcrypt');
+const { send } = require('process');
 // const crypto = require('crypto');
 
 const salt = 12;
@@ -32,6 +33,31 @@ app.use(cors());
 //메인페이지 접속시 build 폴더의 index.html을 보내줘!
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/variousproj/build/index.html'));
+});
+
+app.post('/', async (req, res) => {
+  let sendData = '';
+  const clientLogin = req.body.loginDatas;
+  console.log(clientLogin);
+
+  const userDataCheck = await User.findOne({
+    where: { emailuser: clientLogin[0] },
+  });
+
+  const match = await bcrypt.compare(
+    clientLogin[1],
+    userDataCheck.passworduser
+  );
+
+  if (match === true) {
+    sendData = 'success';
+    res.send(sendData);
+  } else if (match === false) {
+    sendData = 'fail';
+    res.send(sendData);
+  }
+  //이메일이  없으면 null값으로 나온다.
+  // console.log('조회된 데이터는 ?', userDataCheck);
 });
 
 app.post('/signUp', async (req, res) => {
